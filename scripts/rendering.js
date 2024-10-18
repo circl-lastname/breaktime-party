374aeab7e388f8131367bf773f693a25c6b31fef
circl <circl.lastname@gmail.com>
2024-10-18 22:03:19 +0200

    Change scaling logic to pretend to be a 1280x720 display

--- scripts/rendering.js
+++ scripts/rendering.js
@@ -12,16 +12,16 @@ rendering.initializeCanvas = function () {
   this.canvas.width = window.innerWidth;
   this.canvas.height = window.innerHeight;
   
-  let horizontalScale = this.canvas.width/32;
-  let optimalHeight = horizontalScale*18;
+  let horizontalScale = this.canvas.width/1280;
+  let optimalHeight = horizontalScale*720;
   
   if (this.canvas.height >= optimalHeight) {
     this.scale = horizontalScale;
     this.originX = 0;
     this.originY = (this.canvas.height/2 - optimalHeight/2) / this.scale;
   } else {
-    this.scale = this.canvas.height/18;
-    let optimalWidth = this.scale*32;
+    this.scale = this.canvas.height/720;
+    let optimalWidth = this.scale*1280;
     this.originX = (this.canvas.width/2 - optimalWidth/2) / this.scale;
     this.originY = 0;
   }
@@ -30,19 +30,20 @@ rendering.initializeCanvas = function () {
   this.canvasHeight = this.canvas.height / this.scale;
   
   this.ctx.scale(this.scale, this.scale);
+  this.ctx.font = "40px Grandstander, sans-serif";
+  this.ctx.textBaseline = "top";
   
   this.ctx.fillStyle = "#202020";
   this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
   
   this.ctx.fillStyle = "#ffffff";
-  this.ctx.fillRect(this.originX, this.originY, 32, 18);
+  this.ctx.fillRect(this.originX, this.originY, 1280, 720);
   
   this.ctx.fillStyle = "#ff0000";
-  this.ctx.fillRect(this.originX+1, this.originY+1, 1, 1);
+  this.ctx.fillRect(this.originX+32, this.originY+32, 32, 32);
   
   this.ctx.fillStyle = "#000000";
-  this.ctx.font = "1px Grandstander, sans-serif";
-  this.ctx.fillText("Hello, World!", this.originX+3, this.originY+3);
+  this.ctx.fillText("Hello, World!", this.originX+64, this.originY+32);
 };
 
 window.addEventListener("resize", () => {
