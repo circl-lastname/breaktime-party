f2985ba3833a4fcb26fa8c025903671aebec9a16
circl <circl.lastname@gmail.com>
2024-10-18 17:20:51 +0200

    Add a basic 16:9 forcing test

---
+++ scripts/rendering.js
@@ -0,0 +1,48 @@
+const rendering = {};
+
+rendering.canvas = document.getElementById("canvas");
+rendering.ctx = canvas.getContext("2d");
+rendering.scale = undefined;
+rendering.originX = undefined;
+rendering.originY = undefined;
+rendering.canvasWidth = undefined;
+rendering.canvasHeight = undefined;
+
+rendering.initializeCanvas = function () {
+  this.canvas.width = window.innerWidth;
+  this.canvas.height = window.innerHeight;
+  
+  let horizontalScale = this.canvas.width/32;
+  let optimalHeight = horizontalScale*18;
+  
+  if (this.canvas.height >= optimalHeight) {
+    this.scale = horizontalScale;
+    this.originX = 0;
+    this.originY = (this.canvas.height/2 - optimalHeight/2) / this.scale;
+  } else {
+    this.scale = this.canvas.height/18;
+    let optimalWidth = this.scale*32;
+    this.originX = (this.canvas.width/2 - optimalWidth/2) / this.scale;
+    this.originY = 0;
+  }
+  
+  this.canvasWidth = this.canvas.width / this.scale;
+  this.canvasHeight = this.canvas.height / this.scale;
+  
+  this.ctx.scale(this.scale, this.scale);
+  
+  this.ctx.fillStyle = "#202020";
+  this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+  
+  this.ctx.fillStyle = "#ffffff";
+  this.ctx.fillRect(this.originX, this.originY, 32, 18);
+  
+  this.ctx.fillStyle = "#ff0000";
+  this.ctx.fillRect(this.originX+1, this.originY+1, 1, 1);
+};
+
+window.addEventListener("resize", () => {
+  rendering.initializeCanvas();
+});
+
+rendering.initializeCanvas();
